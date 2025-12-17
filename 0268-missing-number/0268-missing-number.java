64cedfc7532a92dd18227f4970133978bf4dc04a
Prince Gupta <pg7835918@gmail.com>
2025-12-17 22:58:58 +0530

    Time: 8 ms (15.25%), Space: 48.3 MB (5.94%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,29 +1,29 @@
 class Solution {
     public int missingNumber(int[] nums) {
-    //   int n=nums.length;
-    //    Arrays.sort(nums);
-    //   if(nums[0]!=0){
-    //     return 0;
-    //       }
-     
-    //   for(int i=0;i<n-1;i++){
-    //     if (nums[i+1] - nums[i] == 2){
-    //         return nums[i]+1;
-    //     }
-    //   } 
-    //   return nums[n-1]+1; 
-
-    Arrays.sort(nums);
-    if(nums[0]!=0){
+      int n=nums.length;
+       Arrays.sort(nums);
+      if(nums[0]!=0){
         return 0;
-    }
-    int j=0;
-    for(int i=0;i<nums.length;i++){
-        if(nums[i]!=j){
-            return j;
+          }
+     
+      for(int i=0;i<n-1;i++){
+        if (nums[i+1] - nums[i] == 2){
+            return nums[i]+1;
         }
-        j++;
-    }
-    return j;
+      } 
+      return nums[n-1]+1; 
+
+    // Arrays.sort(nums);
+    // if(nums[0]!=0){
+    //     return 0;
+    // }
+    // int j=0;
+    // for(int i=0;i<nums.length;i++){
+    //     if(nums[i]!=j){
+    //         return j;
+    //     }
+    //     j++;
+    // }
+    // return j;
     }
 }
