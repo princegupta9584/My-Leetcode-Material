4b70f614693baacfc9ee460f5254cf95be9f834d
Prince Gupta <pg7835918@gmail.com>
2026-01-09 10:09:12 +0530

    Time: 0 ms (100%), Space: 48.4 MB (33.46%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start=0;
+        int last=nums.length-1;
+        while(start<=last){
+            int mid=(start+last)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                last=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return -1;
+    }
+}
