0c8b8ed223a50d305d0665e0f74b2302d9d31b39
Prince Gupta <pg7835918@gmail.com>
2026-01-09 09:57:21 +0530

    Time: 0 ms (100%), Space: 44.5 MB (58.86%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxsum=0;
+        for(int i=0;i<accounts.length;i++){
+            int sum=0;
+            for(int j=0;j<accounts[i].length;j++){
+                sum+=accounts[i][j];
+            }
+            maxsum=Math.max(sum,maxsum);
+        }
+        return maxsum;
+    }
+}
