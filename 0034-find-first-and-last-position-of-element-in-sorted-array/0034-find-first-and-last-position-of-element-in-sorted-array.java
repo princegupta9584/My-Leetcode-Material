426324bd9cf74ea69d8c12371b70058383904ac0
Prince Gupta <pg7835918@gmail.com>
2026-01-12 16:30:20 +0530

    Time: 0 ms (100%), Space: 48 MB (87.95%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -33,5 +33,39 @@ class Solution {
             }
         } 
         return new int[]{lower,upper};
+
+        // int start=0;
+        // int end=nums.length-1;
+        // int upper=-1;
+        // int lower=-1;
+        // while(start<=end){
+        //     int mid=start+(end-start)/2;
+        //     if(nums[mid]==target){
+        //         upper=mid;
+        //         end=mid-1;
+        //     }
+        //     else if(nums[mid]>target){
+        //         end=mid-1;
+        //     }
+        //     else{
+        //         start=mid-1;
+        //     }
+        // }
+        // start=0;
+        // end=nums.length-1;
+        // while(start<=end){
+        //     int mid=start+(end-start)/2;
+        //     if(nums[mid]==target){
+        //         lower=mid;
+        //         start=mid+1;
+        //     }
+        //     else if(nums[mid]>target){
+        //         end=mid-1;
+        //     }
+        //     else{
+        //         start=mid-1;
+        //     }
+        // }
+        // return new int[]{upper,lower};
     }
 }
