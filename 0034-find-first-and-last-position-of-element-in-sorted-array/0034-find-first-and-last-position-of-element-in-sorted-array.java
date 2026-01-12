ddf04b0bb8fc03ae0fe617f3e1cffeebb8a78541
Prince Gupta <pg7835918@gmail.com>
2026-01-12 16:36:38 +0530

    Time: 0 ms (100%), Space: 48.2 MB (34.38%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,71 +1,71 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int start=0;
-        int last=nums.length-1;
-        int lower=-1;
-        int upper=-1;
-        while(start<=last){
-            int mid=start+(last-start)/2;
-            if(nums[mid]==target){
-                lower=mid;
-                last=mid-1;
-            }
-            else if(nums[mid]<target){
-                start=mid+1;
-            }
-            else{
-                last=mid-1;
-            }
-        } 
-        start=0;
-        last=nums.length-1;
-        while(start<=last){
-            int mid=start+(last-start)/2;
-            if(nums[mid]==target){
-                upper=mid;
-                start=mid+1;
-            }
-            else if(nums[mid]<target){
-                start=mid+1;
-            }
-            else{
-                last=mid-1;
-            }
-        } 
-        return new int[]{lower,upper};
-
         // int start=0;
-        // int end=nums.length-1;
-        // int upper=-1;
+        // int last=nums.length-1;
         // int lower=-1;
-        // while(start<=end){
-        //     int mid=start+(end-start)/2;
-        //     if(nums[mid]==target){
-        //         upper=mid;
-        //         end=mid-1;
-        //     }
-        //     else if(nums[mid]>target){
-        //         end=mid-1;
-        //     }
-        //     else{
-        //         start=mid-1;
-        //     }
-        // }
-        // start=0;
-        // end=nums.length-1;
-        // while(start<=end){
-        //     int mid=start+(end-start)/2;
+        // int upper=-1;
+        // while(start<=last){
+        //     int mid=start+(last-start)/2;
         //     if(nums[mid]==target){
         //         lower=mid;
+        //         last=mid-1;
+        //     }
+        //     else if(nums[mid]<target){
         //         start=mid+1;
         //     }
-        //     else if(nums[mid]>target){
-        //         end=mid-1;
+        //     else{
+        //         last=mid-1;
+        //     }
+        // } 
+        // start=0;
+        // last=nums.length-1;
+        // while(start<=last){
+        //     int mid=start+(last-start)/2;
+        //     if(nums[mid]==target){
+        //         upper=mid;
+        //         start=mid+1;
+        //     }
+        //     else if(nums[mid]<target){
+        //         start=mid+1;
         //     }
         //     else{
-        //         start=mid-1;
+        //         last=mid-1;
         //     }
-        // }
-        // return new int[]{upper,lower};
+        // } 
+        // return new int[]{lower,upper};
+
+        int start=0;
+        int end=nums.length-1;
+        int upper=-1;
+        int lower=-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                upper=mid;
+                end=mid-1;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        start=0;
+        end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                lower=mid;
+                start=mid+1;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return new int[]{upper,lower};
     }
 }
