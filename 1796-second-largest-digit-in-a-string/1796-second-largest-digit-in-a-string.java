f4ee2af4dfb4bcc6162f4202cc83be378f9924f3
Prince Gupta <pg7835918@gmail.com>
2026-01-30 23:48:15 +0530

    Time: 2 ms (58.52%), Space: 43.2 MB (89.51%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
@@ -1,20 +1,41 @@
 class Solution {
     public int secondHighest(String s) {
-        int largest = -1;
-        int second = -1;
+        // int largest = -1;
+        // int second = -1;
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+        // for (int i = 0; i < s.length(); i++) {
+        //     char ch = s.charAt(i);
 
-            if (Character.isDigit(ch)) {
-                int num = ch - '0';
+        //     if (Character.isDigit(ch)) {
+        //         int num = ch - '0';
 
-                if (num > largest) {
-                    second = largest;
-                    largest = num;
-                } 
-                else if (num < largest && num > second) {
-                    second = num;
+        //         if (num > largest) {
+        //             second = largest;
+        //             largest = num;
+        //         } 
+        //         else if (num < largest && num > second) {
+        //             second = num;
+        //         }
+        //     }
+        // }
+        // return second;
+
+
+        int largest=-1;
+        int second=-1;
+
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+
+            if(Character.isDigit(ch)){
+                int num=ch-'0';
+
+                if(num>largest){
+                    second=largest;
+                    largest=num;
+                }
+                else if(num<largest && num>second){
+                    second=num;
                 }
             }
         }
