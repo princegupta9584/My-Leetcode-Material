3c8560da59433146e0d9d7d8a8feb25a8affbb6d
Prince Gupta <pg7835918@gmail.com>
2026-02-20 15:10:02 +0530

    Time: 1 ms (93.7%), Space: 43.1 MB (39.67%) - LeetHub

---
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String freqAlphabets(String s) {
+        
+        StringBuilder ans = new StringBuilder();
+        int i = 0;
+        
+        while (i < s.length()) {
+            
+            // Check if two-digit + '#' pattern
+            if (i + 2 < s.length() && s.charAt(i + 2) == '#') {
+                
+                int val = Integer.parseInt(s.substring(i, i + 2));
+                ans.append((char) ('a' + val - 1));
+                i += 3;
+                
+            } else {
+                
+                int val = s.charAt(i) - '0';
+                ans.append((char) ('a' + val - 1));
+                i++;
+            }
+        }
+        
+        return ans.toString();
+    }
+}
