9beb172d275a44cd3b006068d22beba64b7d59c7
Prince Gupta <pg7835918@gmail.com>
2026-03-01 22:06:06 +0530

    Time: 4 ms (44.02%), Space: 43.6 MB (11.46%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -2,32 +2,32 @@ import java.util.Arrays;
 
 class Solution {
     public int specialArray(int[] nums) {
-        // Arrays.sort(nums);
-        // int n = nums.length;
-        
-        // for(int i = 0; i < n; i++) {
-        //     int x = n - i;
-            
-        //     if(nums[i] >= x && (i == 0 || nums[i - 1] < x)) {
-        //         return x;
-        //     }
-        // }
-        
-        // return -1;
-
-        
+        Arrays.sort(nums);
         int n = nums.length;
-        for (int x = 1; x <= n; x++) {
-            int count = 0;
-            for (int i = 0; i < n; i++) {
-                if (nums[i] >= x) {
-                    count++;
-                }
-            }
-            if (count == x) {
+        
+        for(int i = 0; i < n; i++) {
+            int x = n - i;
+            
+            if(nums[i] >= x && (i == 0 || nums[i - 1] < x)) {
                 return x;
             }
         }
+        
         return -1;
+
+        
+        // int n = nums.length;
+        // for (int x = 1; x <= n; x++) {
+        //     int count = 0;
+        //     for (int i = 0; i < n; i++) {
+        //         if (nums[i] >= x) {
+        //             count++;
+        //         }
+        //     }
+        //     if (count == x) {
+        //         return x;
+        //     }
+        // }
+        // return -1;
     }
 }
