d2f4f4c39a70528333d035857b2ea5974050aa46
Prince Gupta <pg7835918@gmail.com>
2026-03-19 13:20:04 +0530

    Time: 82 ms (95.84%), Space: 51.2 MB (49.28%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -3,7 +3,7 @@ class Solution {
         boolean primes[]=new boolean[n+1];
         for(int i=2;i*i<=n;i++){
             if(!primes[i]){
-                for(int j=i*2;j<n;j+=i){
+                for(int j=i*i;j<n;j+=i){
                     primes[j]=true;
                 }
             }
