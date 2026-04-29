05058d5c1d9e39b8af7439a9ce33f0e9646c78fe
Prince Gupta <pg7835918@gmail.com>
2026-04-29 21:40:24 +0530

    Time: 12 ms (46.31%), Space: 46.1 MB (64.49%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+         int l=0;
+        int r=0;
+        int maxlen=0;
+        int maxfreq=0;
+        int[] freq=new int[26];
+        while(r<s.length()){
+            freq[s.charAt(r)-'A']++;
+            maxfreq=Math.max(maxfreq,freq[s.charAt(r)-'A']);
+            if(((r-l+1)-maxfreq)>k){
+                freq[s.charAt(l)-'A']--;
+                l++;
+            }
+            maxlen=Math.max(maxlen,r-l+1);
+            r++;
+        }
+        return maxlen;
+    }
+}
