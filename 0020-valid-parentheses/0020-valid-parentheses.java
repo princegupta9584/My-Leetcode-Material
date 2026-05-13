6477643e96a6ccb640b040439949cc865f3dbea4
Prince Gupta <pg7835918@gmail.com>
2026-05-13 18:32:39 +0530

    Time: 3 ms (87.74%), Space: 43.3 MB (57.09%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,20 +1,49 @@
 class Solution {
     public boolean isValid(String s) {
-        String sc="";
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='{' || s.charAt(i)=='[' || s.charAt(i)=='('){
-                sc+=s.charAt(i);
-            }
-            else{
-                if(sc.length() == 0) return false;
-                if((s.charAt(i)=='}') && (sc.charAt(sc.length()-1)=='{') || (s.charAt(i)==']') && (sc.charAt(sc.length()-1)=='[') || (s.charAt(i)==')') && (sc.charAt(sc.length()-1)=='(')){
-                    sc = sc.substring(0, sc.length() - 1);
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch =='['){
+                stack.push(ch);
+            }else{
+                if(ch == ')'){
+                    if(stack.empty() || stack.pop() != '('){
+                        return false;
+                    }
                 }
-                else{
-                    return false;
+
+                if(ch == '}'){
+                    if(stack.empty() || stack.pop() != '{'){
+                        return false;
+                    }
+                }
+
+                if(ch == ']'){
+                    if(stack.empty() || stack.pop() != '['){
+                        return false;
+                    }
                 }
             }
+           
         }
-        return sc.length()==0;
+         return stack.empty();
     }
 }
+
+
+//******************************* Normal Approach **************************** */
+        // String sc="";
+        // for(int i=0;i<s.length();i++){
+        //     if(s.charAt(i)=='{' || s.charAt(i)=='[' || s.charAt(i)=='('){
+        //         sc+=s.charAt(i);
+        //     }
+        //     else{
+        //         if(sc.length() == 0) return false;
+        //         if((s.charAt(i)=='}') && (sc.charAt(sc.length()-1)=='{') || (s.charAt(i)==']') && (sc.charAt(sc.length()-1)=='[') || (s.charAt(i)==')') && (sc.charAt(sc.length()-1)=='(')){
+        //             sc = sc.substring(0, sc.length() - 1);
+        //         }
+        //         else{
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return sc.length()==0;
