abd5e46d27371cc6ef89dc9f3e41a8d2464c7bda
Prince Gupta <pg7835918@gmail.com>
2026-05-15 17:59:19 +0530

    Time: 1559 ms (9.97%), Space: 46.3 MB (41.6%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        String ans = "";
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+                if(palindrome(s, i, j)){
+                     if((j - i + 1) > ans.length()) {
+                        ans = s.substring(i, j + 1);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public boolean palindrome(String s, int start, int end){
+
+        while(start<end){
+            if(s.charAt(start)!=(s.charAt(end))){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
