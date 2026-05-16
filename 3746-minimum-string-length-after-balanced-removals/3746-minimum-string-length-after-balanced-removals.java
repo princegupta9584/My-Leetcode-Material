599d7496fe3e4accd2d9e38bb52819b9311e3b50
Prince Gupta <pg7835918@gmail.com>
2026-05-16 20:04:14 +0530

    Time: 3 ms (97.5%), Space: 47.4 MB (21.39%) - LeetHub

--- 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.java
+++ 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.java
@@ -1,21 +1,35 @@
 class Solution {
     public int minLengthAfterRemovals(String s) {
-        Stack<Character> stacka = new Stack<>();
-        Stack<Character> stackb = new Stack<>();
+        // Stack<Character> stacka = new Stack<>();
+        // Stack<Character> stackb = new Stack<>();
+        // for(char ch : s.toCharArray()){
+        //     if(ch=='a'){
+        //         stacka.push(ch);
+        //     }else{
+        //         stackb.push(ch);
+        //     }
+        // }
+        // int ans = 0;
+        // if(stacka.size()>stackb.size()){
+        //     ans = stacka.size()-stackb.size();
+        // }
+        // if(stackb.size()>stacka.size()){
+        //     ans = stackb.size()-stacka.size();
+        // }
+        // return ans;
+
+        int a = 0;
+        int b = 0;
+
         for(char ch : s.toCharArray()){
-            if(ch=='a'){
-                stacka.push(ch);
+
+            if(ch == 'a'){
+                a++;
             }else{
-                stackb.push(ch);
+                b++;
             }
         }
-        int ans = 0;
-        if(stacka.size()>stackb.size()){
-            ans = stacka.size()-stackb.size();
-        }
-        if(stackb.size()>stacka.size()){
-            ans = stackb.size()-stacka.size();
-        }
-        return ans;
+
+        return Math.abs(a - b);
     }
 }
