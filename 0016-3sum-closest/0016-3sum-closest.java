967c46c1d4396764c25ea58bd9ca3a1291a90c33
Prince Gupta <pg7835918@gmail.com>
2026-05-18 23:50:17 +0530

    Time: 17 ms (78%), Space: 45.5 MB (48.37%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,20 +1,48 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-
+        Arrays.sort(nums);
         int closest = nums[0]+nums[1]+nums[2];
-        for(int i =0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                for(int k =j+1;k<nums.length;k++){
-                    int sum = nums[i]+nums[j]+nums[k];
-                    // int close = Math.abs(target-ans);
-                    if(Math.abs(target-sum) < Math.abs(target-closest)){
-                        closest=sum;
-                    }
+        for(int i=0;i<nums.length-2;i++){
+            int left = i+1;
+            int right = nums.length-1;
+            while(left<right){
+                int sum = nums[i]+nums[left]+nums[right];
+                if(Math.abs(target-sum) < Math.abs(target - closest)){
+                    closest = sum;
+                }
+                if(sum == target){
+                    return sum;
+                }
+                else if(sum<target){
+                    left++;
+                }
+                else{
+                    right--;
                 }
             }
         }
         return closest;
     }
 }
+
+// *************************** Brute Force ****************************
+// class Solution {
+//     public int threeSumClosest(int[] nums, int target) {
+//         int max = Integer.MIN_VALUE;
+//         int min = Integer.MAX_VALUE;
+
+//         int closest = nums[0]+nums[1]+nums[2];
+//         for(int i =0;i<nums.length;i++){
+//             for(int j=i+1;j<nums.length;j++){
+//                 for(int k =j+1;k<nums.length;k++){
+//                     int sum = nums[i]+nums[j]+nums[k];
+//                     // int close = Math.abs(target-ans);
+//                     if(Math.abs(target-sum) < Math.abs(target-closest)){
+//                         closest=sum;
+//                     }
+//                 }
+//             }
+//         }
+//         return closest;
+//     }
+// }
