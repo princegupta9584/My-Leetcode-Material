b350c459a402abb56e9e534703d1010ab0e586da
Prince Gupta <pg7835918@gmail.com>
2026-07-16 23:31:24 +0530

    Time: 16 ms (92.78%), Space: 45.7 MB (14.07%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,48 +1,42 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
+
         Arrays.sort(nums);
-        int closest = nums[0]+nums[1]+nums[2];
-        for(int i=0;i<nums.length-2;i++){
-            int left = i+1;
-            int right = nums.length-1;
-            while(left<right){
-                int sum = nums[i]+nums[left]+nums[right];
-                if(Math.abs(target-sum) < Math.abs(target - closest)){
-                    closest = sum;
+
+        int minDiff = Integer.MAX_VALUE;
+        int resultSum = 0;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+                int diff = Math.abs(sum - target);
+
+                // Agar current sum target ke aur close hai
+                if (diff < minDiff) {
+                    minDiff = diff;
+                    resultSum = sum;
                 }
-                if(sum == target){
+
+                // Exact target mil gaya
+                if (sum == target) {
                     return sum;
                 }
-                else if(sum<target){
+                // Sum chhota hai -> left badhao
+                else if (sum < target) {
                     left++;
                 }
-                else{
+                // Sum bada hai -> right ghatao
+                else {
                     right--;
                 }
             }
         }
-        return closest;
+
+        return resultSum;
     }
 }
-
-// *************************** Brute Force ****************************
-// class Solution {
-//     public int threeSumClosest(int[] nums, int target) {
-//         int max = Integer.MIN_VALUE;
-//         int min = Integer.MAX_VALUE;
-
-//         int closest = nums[0]+nums[1]+nums[2];
-//         for(int i =0;i<nums.length;i++){
-//             for(int j=i+1;j<nums.length;j++){
-//                 for(int k =j+1;k<nums.length;k++){
-//                     int sum = nums[i]+nums[j]+nums[k];
-//                     // int close = Math.abs(target-ans);
-//                     if(Math.abs(target-sum) < Math.abs(target-closest)){
-//                         closest=sum;
-//                     }
-//                 }
-//             }
-//         }
-//         return closest;
-//     }
-// }
